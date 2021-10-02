f729b3fcac4b6c48d3b9f9e89c966b78e280719e
KrishnaTiwariCoder <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2021-10-02 15:50:39 +0530

    i know this bot having some bugs but try it it solves most of the problem :)

--- src/index.js
+++ src/index.js
@@ -18,26 +18,44 @@ let page = null;
   });
 
   await page.goto("https://instagram.com", { waitUntil: "networkidle2" });
+  await page.waitForSelector(`input[name="username"]`, {
+    visible: true,
+  });
+  await page.type(`input[name="username"]`, username, { delay: 30 });
+  await page.type(`input[name="password"]`, password, { delay: 30 });
 
-  await page.type(`input[name="username"]`, username, { delay: 25 });
-  await page.type(`input[name="password"]`, password, { delay: 25 });
   await page.keyboard.press("Enter");
   await page.waitForTimeout(5000);
   const [button] = await page.$x("//button[contains(., 'Not Now')]");
   if (button) {
     await button.click();
   }
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(3000);
+
   const [button2] = await page.$x("//button[contains(., 'Not Now')]");
   if (button2) {
     await button2.click();
   }
+  await page.waitForSelector(`input[placeholder="Search"]`, {
+    visible: true,
+  });
 
   await page.type(`input[placeholder="Search"]`, "reels");
-  await page.waitForTimeout(2000);
+
+  await page.waitForSelector(`div[role="none"] a`);
+
   await page.click(`div[role="none"] a`);
+
+  // await page.waitForSelector(`div._9AhH0`);
   await page.waitForTimeout(4000);
   await page.click("div._9AhH0");
-  await page.waitForTimeout(4000);
-  await page.click(`svg[aria-label="Like"]`);
+
+  // await likeAndNext(page);
+  setInterval(async () => {
+    const likeBtn = await page.waitForSelector(`svg[aria-label="Like"]`);
+    if (likeBtn) {
+      await page.click(`svg[aria-label="Like"]`);
+    }
+    await page.click(`a.coreSpriteRightPaginationArrow`);
+  }, 3000);
 })();
